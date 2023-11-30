ec94256bab5653b3c0a8f04e7d32013f03ba2f47
maridai thierry <t.maridadi@alu.student.com>
2023-11-30 14:24:50 +0000

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,12 @@
-mport urllib.request
+#!/usr/bin/python3
+'''value of the X-Request-Id variable found in the header'''
+
+
+import urllib.request
 import sys
 
+
 if __name__ == '__main__':
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        header = response.info()
-        print(header["X-Request-Id"])
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        header = res.info()
+        print(header['X-Request-Id'])
