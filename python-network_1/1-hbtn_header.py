9b31bbb6f474159050496a824cf12ea154dcce1b
maridai thierry <t.maridadi@alu.student.com>
2023-11-30 14:21:52 +0000

    1-hbtn_header.py

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,7 @@
+mport urllib.request
+import sys
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header["X-Request-Id"])
