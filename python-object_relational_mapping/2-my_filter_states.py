df8a8cf2577ba87d0dfdf5883da147c02ff74113
Maridadi Thierry <t.maridadi>
2023-12-13 04:34:08 -0800

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,50 +1,37 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""  lists all states from the database hbtn_0e_0_usa """
+""" takes in an argument and displays all
+values in the states table
+of hbtn_0e_0_usa where name matches the argument
+"""
+
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
-=======
-"""
-    script that takes in an argument and displays all
-    values in the states table of hbtn_0e_0_usa
-"""
+def select_states_with_names():
+    """ takes in an argument and displays all
+    values in the states table
+    of hbtn_0e_0_usa where name matches the argument
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cursor = db.cursor()
 
-import sys
-import MySQLdb
+    cursor \
+        .execute("SELECT * FROM states WHERE BINARY name='{:s}'\
+                ORDER BY id ASC".format(sys.argv[4]))
 
-
-if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        user=sys.argv[1],
-        password=sys.argv[2],
-        db=sys.argv[3],
-        host="localhost",
-        port=3306
-    )
-    cursor = conn.cursor()
-    sql = """ SELECT * FROM states
-        WHERE name LIKE BINARY '{}'
-        ORDER BY id ASC """.format(sys.argv[4])
-
-    cursor.execute(sql)
-    data = cursor.fetchall()
-
-    for row in data:
-        print(row)
+    records = cursor.fetchall()
+    for data in records:
+        print(data)
 
     cursor.close()
-    conn.close()
->>>>>>> ca8a0c3d0449bd06a28081efb6904e29e2f97e70
+    db.close()
+
+
+if __name__ == "__main__":
+    select_states_with_names()
