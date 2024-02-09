5201e394a5c612e38ad9c1f1c6ed9fbe2a2e8143
Maridadi Thierry <t.maridadi>
2024-02-08 23:37:19 -0800

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, contents) => {
-  err ? console.log(err) : console.log(contents.toString());
+const request = require('fs');
+
+const filePath = process.argv[2];
+
+request.readFile(filePath, function (err, data) {
+  if (err) console.log(err);
+  const content = data;
+  console.log(content.toString());
 });
